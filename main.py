d855fb775a9b0cad5e1187ae2bbc79586b31d838
Emre Seferoğlu <yunusemre5331@icloud.com>
2025-09-03 22:47:38 +0300

    Implement web scraping functionality to extract headlines from a specified URL

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello emoş")
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://seferogluemre.vercel.app/"
+response=requests.get(url)
+
+soup=BeautifulSoup(response.text,"html.parser")
+
+headlines=soup.find_all('h2')
+
+print("Scraping Script Started....")
+
+for h in headlines[:10]:
+    print("-",h.text.strip())
